6fb1f108bb4514c5455bcd3d37f5e858549d550e
pavandwivedi <124969756+pavandwivedi@users.noreply.github.com>
2024-01-08 14:43:28 +0530

    Update configDB.js

--- src/config/configDB.js
+++ src/config/configDB.js
@@ -4,12 +4,7 @@ import dotenv from "dotenv"
 dotenv.config();
 export default async function connectDB(){
     try {
-        const DB_OPTIONS = {
-   dbName:process.env.DBNAME,
-   user:process.env.DBUSERNAME,
-   pass:process.env.DBPASSWORD,
-   authSource:process.env.DBAUTHSOURCE
- }
+
         const connect =  await mongoose.connect(  process.env.mongoURL);
         console.log('DB connected! '+connect.connection.host);
     } catch (error) {
